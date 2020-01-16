80b19184cd7c4ee9d316159d868d1a823b5eaa45
RenChu Wang <patrick1031wang@gmail.com>
2020-01-16 16:36:51 +0800

    not finished yet

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # agymc
 An asynchronous wrapper for OpenAI Gym library
+
+**My head hurts right now. It will be finished tonight, or the night after.**
--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 import setuptools
 
+with open(file="./README.md", mode="r") as readme:
+    long_descrition = readme.read()
 
-setuptools.setup()
+setuptools.setup(
+    name="agymc",
+    version="0.1",
+    author="MutatedFlood",
+    author_email="b06901038@g.ntu.edu.tw",
+    url="https://github.com/MutatedFlood/agymc",
+    install_requires=("gym",),
+    description="An OpenAI wrapper for concurrent batch support",
+    long_descrition=long_descrition,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+)
