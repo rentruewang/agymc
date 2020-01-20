33af133fe9c1924a51618e3852040ef6b682ef17
RenChu Wang <patrick1031wang@gmail.com>
2020-01-20 23:22:28 +0800

    setup

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ setuptools.setup(
     author_email="b06901038@ntu.edu.tw",
     license="MIT",
     url="https://github.com/MutatedFlood/agymc",
-    install_requires=("gym",),
+    install_requires=["gym"],
     description="A concurrent wrapper for OpenAI Gym library that runs multiple environments concurrently.",
     long_description=long_descrition,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     python_requires=">=3.7",
-    classifiers=("Programming Language :: Python :: 3.7",),
+    classifiers=["Programming Language :: Python :: 3.7"],
 )
